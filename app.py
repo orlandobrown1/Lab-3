17f9d013e04bdbfffdb729e25d154a69fc3087f4
orlando brown <orlandobrown205@gmail.come.com>
2023-02-11 18:14:35 -0500

    Added delete & patch request

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ pydantic.json.ENCODERS_BY_TYPE[ObjectId]= str
 
 @app.get("/profile")
 async def get_all_profiles():
-    profiles= await db["profile"].find().to_list(999)
+    profiles= await db["profile"].find().to_list(1)
 
     return profiles
 
@@ -35,24 +35,24 @@ async def create_new_profile(request: Request):
 
 
 
-    new_profile = await db["profile"].insert_one(todo_object)
-    created_profile = await db["profile"].find_one({"last_updated": new_profile.insert_last_updated})
+    new_profile = await db["profile"].insert_one(tank_object)
+    created_profile = await db["profile"].find_one({"_id": new_profile.inserted_id})
 
     return created_profile 
 
 
-    @app.get("profile/{data}")
-    async def get_all_profiles_by_data(data:str):
-        profile= await db["profile"].find_one({"_data":
-        ObjectId(data)})
-        return profile
+@app.get("profile/{data}")
+async def get_all_profiles_by_data(data:str):
+    profile= await db["profile"].find_one({"_data":
+    ObjectId(data)})
+    return profile
 
 @app.post("profile/{data}")
 async def create_new_profile_by_data(request: Request):
     tank_object= await request.json()
 
     new_profile= await db["profile"].insert_one(tank_objects)
-    created_profiles= await db["profile"].find_one({"last_updated":new_profiles.insert_last_updated})
+    created_profiles= await db["profile"].find_one({"_id":new_profile.inserted_id})
 
     return created_profiles
 
@@ -60,3 +60,29 @@ async def create_new_profile_by_data(request: Request):
 async def retrive_tanks():
     tanks = await db["tank"].find().to_list(999)
     return tanks
+
+@app.delete("/data/{id}", status_code=204)
+async def delete_tank(id: str):
+
+    found= await db["tank"].find_one({"_id": ObjectId(id)})
+    if (found) is None:
+        raise HTTPException(status_code=404, detail="item not found")
+
+        remove_tank= await db["tank"].delete_one({"_id": ObjectId(id)})
+
+        return {"message": "ITEM WAS DELETED"}
+
+
+@app.patch("/data/{id}")
+async def do_update(id: str, request: Request):
+    updated= await request.json()
+    request= await db["tank"].update_one({"_id":ObjectId(id)}, {'$set': updated})
+
+    if result.modified_count == 1:
+        if(
+            updated_tank := await db["tank"].find_one({"_id": id})
+        ) is not None:
+            return updated_tank
+
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
